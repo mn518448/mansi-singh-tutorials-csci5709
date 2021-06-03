08678c2b3a4863c5884b27855a094ebbe6cb13f5
Mansi Singh <mansisingh@Mansis-MacBook-Air.local>
2021-06-03 14:40:02 -0500

    Build issue fixed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/tutorial3'));
+app.use(express.static('./dist/angular-web5709Tutorial3'));
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname, '/dist/tutorial3/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/angular-web5709Tutorial3/index.html'));
 });
 app.listen(process.env.PORT || 8080);
